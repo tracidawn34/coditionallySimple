4e5a841045412772de2fe3960948a2e90628cd50
unknown <tmcfall.prsvr@gmail.com>
2023-11-28 12:18:17 -0500

    final

---
+++ main.js
@@ -0,0 +1,86 @@
+/******************************
+ * YOUR CODE GOES RIGHT HERE! *
+ ******************************/
+function isItLong(str) {
+  if (str.length > 20) {
+    return "That's a long string!"
+  }
+}
+function isItMedium(str) {
+  if (str.length >= 10 && str.length <= 20) {
+    return "That's a regular sized string!"
+  }
+}
+
+function isItShort(str) {
+  if (str.length < 10) {
+    return "That's a small string!"
+  } else {
+    return "That's not a small string!"
+  }
+}
+
+function howLongIsMyString(str){
+  if (str.length > 20){
+    return "That's a long string!"
+  }
+  if (str.length >= 10 && str.length <= 20){
+    return "That's a regular sized string!"
+  }
+  if (str.length < 10){
+    return "That's a small string!"
+  }
+}
+
+function personsHeight(instructors){
+if ("Shaq" === instructors){
+  return 85
+}
+if ("Zooey" === instructors){
+  return 65
+}
+else{
+  return "I don't know everyone's height!"
+}
+}
+
+
+
+
+
+
+
+/**********************************
+ *      OUR CODE GOES BELOW.      *
+ * THIS IS FOR INTERNAL USE ONLY. *
+ * DO NOT ADD OR CHANGE ANYTHING! *
+ **********************************/
+
+if (typeof isItLong === 'undefined') {
+  isItLong = undefined;
+}
+
+if (typeof isItMedium === 'undefined') {
+  isItMedium = undefined;
+}
+
+if (typeof isItShort === 'undefined') {
+  isItShort = undefined;
+}
+
+if (typeof howLongIsMyString === 'undefined') {
+  howLongIsMyString = undefined;
+}
+
+if (typeof personsHeight === 'undefined') {
+  personsHeight = undefined;
+}
+
+
+module.exports = {
+  isItLong,
+  isItMedium,
+  isItShort,
+  howLongIsMyString,
+  personsHeight,
+}
